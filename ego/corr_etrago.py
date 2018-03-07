096449bb46a3d220327858f0ee324a4990e0fe21
maltesc <malte.scharf@googlemail.com>
2018-03-07 13:42:26 +0100

    minor stuff

--- ego/corr_etrago.py
+++ ego/corr_etrago.py
@@ -49,17 +49,17 @@ args = {
   'parallelisation': False, # This is OK in my case cause no storage optimization. Macht alles nacheinander
   'pf_post_lopf': False, # Weitere Möglichkeit sind noch solver options
   'reproduce_noise': False, # Das scheint so noch nich zu funkionieren....
-  'results': '~/maltesc/Git/eGo/ego/results',
+  'results': False, #'~/maltesc/Git/eGo/ego/results'
   'scn_name': 'SH Status Quo',
   'skip_snapshots': False,
   'solver': 'gurobi',
   'storage_extendable': False}
 
-args['user_name'] = 'maltesc'
+args['user_name'] = 'malte_scharf'
 ## eTraGo iteration parameters
 b_factor = [10.0]
 snapshots = [(1, 2)]
-comments = ["24 Std NEP 2035 Sever Test"]
+comments = ["2 Std SH Status Quo Sever Test"]
 
 try:
     conn = db.connection(section='oedb')
@@ -84,9 +84,9 @@ for b, s, c in zip(b_factor, snapshots, comments):
         eTraGo = etrago(args)
     except:
         logger.error('eTraGo returned Error',  exc_info=True)
-    try:
-        logger.info('Results to DB')
-        results_to_oedb(session, eTraGo, args)
-
-    except:
-        logger.error('Could not save Results to DB',  exc_info=True)
+#    try:
+#        logger.info('Results to DB')
+#        results_to_oedb(session, eTraGo, args)
+#
+#    except:
+#        logger.error('Could not save Results to DB',  exc_info=True)
